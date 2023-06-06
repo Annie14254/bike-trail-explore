1aa9c4a617ab321098106448a45600f91fd53840
unknown <annieyashley@gmail.com>
2023-06-06 11:20:12 -0500

    merging with main

--- server/controllers/user-controllers.js
+++ server/controllers/user-controllers.js
@@ -33,7 +33,42 @@ module.exports = {
         }
         console.log(oneUser)
         res.status(200).json(oneUser)
-    }
+    },
+
+    async authUser({ body }, res) {
+        let user
+        try {
+          user = await User.findOne({ email: body.email});
+        } catch(err){
+          return res.status(400).json({ message: 'Unable to authenticate user' });
+        }
+        if (!user) return res.status(400).json({ message: 'Unable to authenticate user' });
+    
+        const isValid = await user.verify(body.password)
+        if( !isValid ) return res.status(400).json({ message: 'Unable to authenticate user' });
+    
+        const token = jwt.sign({
+          email: user.email,
+          id: user._id
+        }, process.env.JWT_SECRET)
+    
+        const { password, ...modifiedUser } = user;
+        res.cookie("auth-cookie", token).json({ status: "success", payload: modifiedUser })
+      },
+    
+    
+      async verifyUser(req, res){
+        const cookie = req.cookies["auth-cookie"]
+        if( !cookie ) return res.status(401).json({msg: "un-authorized"})
+    
+        const isVerified = jwt.verify(cookie, process.env.JWT_SECRET)
+        if( !isVerified ) return res.status(401).json({msg: "un-authorized"})
+    
+        const user = await User.findOne({ _id: isVerified.id }).select("-password")
+        if( !user ) return res.status(401).json({msg: "authorized"})
+        
+        return res.status(200).json({ status: "success", payload: user })
+      }
 
 //   create controller routes for update and delete
 };

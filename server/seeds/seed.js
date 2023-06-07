33b94929428ebb3b691b3bc82e8d373bc51e56e5
unknown <annieyashley@gmail.com>
2023-06-07 12:04:20 -0500

    adds framework to retrieve data from strava

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const Trail = require("../models/Trail");
 const db = require("../config/connection").mongoURI;
 const fetch = require("node-fetch");
+var StravaApiV3 = require('strava_api_v3');
 
 let resultData;
 let saveCounter = 0;
@@ -37,3 +38,36 @@ try{
    console.log(error);
 }
 })
+
+
+var defaultClient = StravaApiV3.ApiClient.instance;
+
+// Configure OAuth2 access token for authorization: strava_oauth
+var strava_oauth = defaultClient.authentications['strava_oauth'];
+strava_oauth.accessToken = "23056b86f67625ae9f162e85ac8f16260ba668a5"
+
+var api = new StravaApiV3.ActivitiesApi()
+
+var id = 789; // {Long} The identifier of the activity.
+
+var opts = { 
+  'includeAllEfforts': true // {Boolean} To include all segments efforts.
+};
+
+var callback = function(error, data, response) {
+  if (error) {
+    console.error(error);
+  } else {
+    console.log('API called successfully. Returned data: ' + data);
+  }
+};
+api.getActivityById(id, opts, callback);
+
+// https://maps.googleapis.com/maps/api/staticmap
+// ?size=400x400&center=${lat},${lon}&zoom=4
+// &path=weight:3%7Ccolor:black%7Cenc:${polyline}
+// &key=AIzaSyDjUvypn2RUsTLSqsK6kOXCuA--8gSQOEc
+
+// client secret ceb94b4a0bb5aee35dc049b4bb624503586db0df
+// Access token: 23056b86f67625ae9f162e85ac8f16260ba668a5
+// Refresh token: dcfef47c8fa6140d14b76fbfa2c441e29a96e1c0

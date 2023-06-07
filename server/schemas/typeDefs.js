692966c0d275ec4a79e925d8e35b5694c47ae2be
unknown <annieyashley@gmail.com>
2023-06-07 10:20:01 -0500

    merging with main

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,28 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+  type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+  }
+
+  type Review {
+    _id: ID
+    reviewAuthor: [User]
+    reviewText: String
+    createdAt: Date
+  }
+
+  type Trail {
+    _id: ID
+    name: String
+    address: String
+    description: String
+    location: String
+  }
+`;
+
+module.exports = typeDefs;
+

26f03a7155f341e3e028d7ba7b3f07352ac04f4d
unknown <annieyashley@gmail.com>
2023-06-08 12:58:32 -0500

    merging with main

---
+++ client/src/components/MapData.js
@@ -0,0 +1,22 @@
+const mapData = [
+    {
+        lat: 44.991803,
+        lon:  -93.309449,
+        polyline: "miuqGnymxP`Edn@dQ~MbH|ZvX?vR?aEglApFeP`E_N?oc@uOwI_k@_NwXiRmA|k@dCln@k@lF"
+    },
+    {
+        lat: 44.742438,
+        lon: -93.705171
+    },
+    {
+        lat: 44.761876, 
+        lon: -93.352074,
+    },
+    {
+        lat: 45.261542, 
+        lon: -93.067679
+    },
+
+]
+
+module.exports = mapData
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,23 @@
 import React from 'react'
 import MyMap from '../components/MyMap'
+import mapData from '../components/MapData'
 
 function Home() {
   return (
     <div>
       <h1>Home Page</h1>  
       <div id = "mapDisplay">
-
-      <MyMap lat="3.21" lon="1.23" polyline="c|gtGbrt_QnzoAcpwDpgtAz~eBm{u@ra{@nnyZddwBht|Oyr~xAnqv]amteFnjtMo`waHbj_aByz}bC`crnAldfdDv|aO~nohDxhtuCnhnc@ndmJ}citD"/>
+        
+        <div>
+          {mapData.map(mapDatum => {
+            return(
+              <div>
+                <MyMap mapDatum={mapDatum}/>
+              </div>
+            )
+          })}
+        </div>      
+      {/* <MyMap lat="3.21" lon="1.23" polyline="c|gtGbrt_QnzoAcpwDpgtAz~eBm{u@ra{@nnyZddwBht|Oyr~xAnqv]amteFnjtMo`waHbj_aByz}bC`crnAldfdDv|aO~nohDxhtuCnhnc@ndmJ}citD"/> */}
       </div>
     </div>
   )

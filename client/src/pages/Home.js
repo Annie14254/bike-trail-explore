41c2442184e517e3673d22c81162e024bcd998eb
LinkinHuber <127816351+LinkinHuber@users.noreply.github.com>
2023-06-08 11:31:41 -0500

    started on getting the maps api to work

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from 'React'
+import MyMap from '../components/MyMap'
 
 function Home() {
   return (
     <div>
       <h1>Home Page</h1>  
+      <MyMap lat="3.21" lon="1.23"/>
     </div>
   )
 }
+
 
 export default Home

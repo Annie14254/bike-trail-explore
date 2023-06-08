2632704500fb78766c0c21fec16c6e86851f08e6
unknown <annieyashley@gmail.com>
2023-06-08 12:41:53 -0500

    adds basic framework for displaying trail maps

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,7 +5,10 @@ function Home() {
   return (
     <div>
       <h1>Home Page</h1>  
-      <MyMap lat="3.21" lon="1.23"/>
+      <div id = "mapDisplay">
+
+      <MyMap lat="3.21" lon="1.23" polyline="c|gtGbrt_QnzoAcpwDpgtAz~eBm{u@ra{@nnyZddwBht|Oyr~xAnqv]amteFnjtMo`waHbj_aByz}bC`crnAldfdDv|aO~nohDxhtuCnhnc@ndmJ}citD"/>
+      </div>
     </div>
   )
 }

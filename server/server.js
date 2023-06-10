abbf8ad3f0b63ce816b0a4a3c124ef8e38e95f02
unknown <annieyashley@gmail.com>
2023-06-10 11:45:56 -0500

    fixes server js snippet to deploy to heroku

--- server/server.js
+++ server/server.js
@@ -13,8 +13,13 @@ app.use(bodyParser.json())
 app.use(express.json())
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+  app.use(express.static(path.join(__dirname, "..", 'client/build')));
+
+  app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, "..", 'client/build/index.html'))
+  })
 }
+
 
 
 app.use(routes);

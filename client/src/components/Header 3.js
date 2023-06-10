217ae1f208930b404bbd1c9e95a03cf334ba91ab
Tyler Miller <tyguymills@gmail.com>
2023-06-10 15:09:42 -0500

    changed footer and profile pic

---
+++ client/src/components/Header 3.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import "../styles/Header.css";
+import { AiOutlineMenu } from "react-icons/ai";
+
+export default function Header() {
+  const [dropNav, setDropNav] = useState(false);
+  
+  useEffect(() => {
+    setDropNav(false);
+  }, []);
+
+  return (
+    <div className="navbar" id={dropNav ? "open" : "close"}>
+      <div className="toggleButton">
+        <button onClick={() => {setDropNav((prev) => !prev)}}> 
+          <AiOutlineMenu /> 
+        </button>
+      </div>
+
+      <div className="links">
+        
+        <a href={`/`} id="name" >RideGuide</a>
+        <a href={`/maps`} className="navs">Trail</a>
+        <a href={`/profile`} className="navs">Profile</a>
+        <a href={`/signup`} className="navs">Sign Up</a>
+        <a href={`/login`} className="navs">Log In</a>
+
+      </div>
+    </div>
+  )  
+}
--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../styles/profile.css';
+import profilePicture from '../styles/images/fillerbicyclist.jpeg';
+import '../styles/images/mapplaceholderimg.png';
 
-function Profile() {
+
+let profilePic = document.getElementById("profile-pic");
+let inputFile = document.getElementById("input-file");
+
+const ProfilePicPicker = () => {
+  const [profilePic, setProfilePic] = useState(null);
+
+  const handlePicChange = (event) => {
+    const selectedPic = event.target.files[0];
+    setProfilePic(URL.createObjectURL(selectedPic));
+  }
+
+
+
   return (
-    <div>Profile</div>
-  )
-}
+    <div className="container">
+      <div className="row">
+        <div className="col-sm-6">
+          <div className="left">
+            <center>
+              <h2 className="pd" id="username">UserName</h2>
+              <h3 className="pd" id="email">Email</h3>
+              <h3 className="pd" id="location">Location</h3>
+              <button id="updateuserinfo">
+                Update User Information
+              </button>
+            </center>
+          </div>
+        </div>
+        <div className="col-sm-6">
+          <div className="right">
+            <center>
+              <h2 className="pd">Profile Picture</h2>
+            </center>
+            <center>
+            <input type="file" accept="image/*" onChange={handlePicChange} />
+            {profilePic && (
+              <img
+                className="profile-pic"
+                src={profilePic}
+                alt="Profile"
+                style={{ height: '10em', width: '10em', borderRadius: '50%' }}
+              />
+            )}
+            </center>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Profile
+
+export default ProfilePicPicker;

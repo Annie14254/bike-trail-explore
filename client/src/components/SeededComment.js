256c5f52c610ac636cb6e8000ea1b30eec973454
unknown <annieyashley@gmail.com>
2023-06-11 18:43:31 -0500

    merging with main

--- client/src/components/SeededComment.js
+++ client/src/components/SeededComment.js
@@ -11,57 +11,38 @@ const SeededComment = (props) => {
   
 
   function fetchReview(){
-    fetch("/api/review",
-    {headers: {
-      'Content-Type': 'application/json'
-    }})
+    fetch("/api/review")
     .then(res =>{
       return res.json()
     })
     .then(data =>{
-      console.log(data)
-      setReviewList(data.allReviews)
+      setReviewList(data)
     })
     .catch(err => console.log(err.message))
   }
+
+  useEffect(() => {
+    fetchReview()
+  }, [])
 
   useEffect(() => {
     console.log(reviewList)
   }, [reviewList])
  
 
-  useEffect(() => {
-    fetchReview()
-  }, [])
-
-
       return(
-        // The actual viewable map
-        // <Col id='card-container'>
-        //   <div className='card-container'>
-        //   {trailsList.map(trail => (
-        //     <Card key={trail._id} className='card'>
-        //       <Card.Img
-        //         variant="top"
-        //           src={`https://maps.googleapis.com/maps/api/staticmap?size=400x400&center=${trail.lat},${trail.lon}&zoom=12&path=weight:3%7Ccolor:red%7Cenc:${trail.polyline}&key=AIzaSyDjUvypn2RUsTLSqsK6kOXCuA--8gSQOEc`}
-        //         />
-        //       <Card.Body>
-        //         <Card.Title>{trail.name}</Card.Title>
-        //         <Card.Text>{trail.description}</Card.Text>
-        //       </Card.Body>
-        //     </Card>
-        //     ))}
-        //   </div>
-        // </Col>
-        reviewList.map(review => (
-            <Card>
-                <Card.Body>
-                    <Card.Title>User</Card.Title>
-                    <Card.Text>Created at: {review.createdAt}</Card.Text>
-                    <Card.Text>{review.reviewText}</Card.Text>
+        <>
+        {reviewList?.map(review => (
+            <Card key = {review?._id} className='comment-card'>
+                <Card.Body className='card-content'>
+                    <Card.Title className='card-content'>User</Card.Title>
+                    <Card.Text>Created at: {review?.createdAt}</Card.Text>
+                    <Card.Text>{review?.reviewText}</Card.Text>
                 </Card.Body>
             </Card>
-        ))
+        
+        ))}
+        </>
       )
   
 }
